1e2712abad2afe766b827d08a20cc3b534b1802d
lama <david3951445@gmail.com>
2022-02-17 18:35:49 +0800

    add ignore

--- test/oneUAV/Simulation/five_UAV_final/Poisson.m
+++
@@ -1,8 +0,0 @@
-function Times = Poisson(lamda,Count)
-    mu = rand(1,Count);
-    Times = 0;
-    for i = 1:Count
-       Times(i+1) = Times(i) + (-log(1-mu(i)))/lamda;
-    end
-    Times = Times(2:length(Times));
-end
