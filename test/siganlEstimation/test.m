bd6433c1a3b38c858e22df57d986f3073b7f294c
lama <david3951445@gmail.com>
2022-03-19 16:11:02 +0800

    matlab robot toolbox practice, RVM2's forward and inverse kinemic success

--- test/siganlEstimation/test.m
+++ test/siganlEstimation/test.m
@@ -1,9 +1,1 @@
 clc; clear; close all
-
-r = 3; % r parameter for chaotic regime
-t = 0 : 0.01 : 100;
-x(1)= 1; % initial value
-for i=1 :length(t)-1
-   x(i+1) = r*x(i)*(1 - x(i));
-end
-plot(t, x)
