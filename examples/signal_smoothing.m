5261e2b21b24ccce7a8c1762542dc4150abc67f9
lama <david3951445@gmail.com>
2023-01-07 17:11:39 +0800

    Refactor

--- examples/signal_smoothing.m
+++ examples/signal_smoothing.m
@@ -1,7 +1,7 @@
 %An example of eliminating the non-smooth properties and outliers that result from numerical differentiation of a signal
 
 clc; clear; close all;
-qr = load('../data/qr.mat').qr;
+qr = load('../src/URTS main script/Robot/data/Robot.mat').qr;
 
 DIM_F   = size(qr, 1);
 dt      = 0.001;
@@ -11,24 +11,52 @@ t       = linspace(0,1,size(qr, 2));
 for i = 1 : DIM_F
 r = qr(i,:);
 
+%% First difference
+% Origin
 dr = diff(r, 1, 2)/dt;
-dr = filloutliers(dr,'clip','movmedian', 10, 'ThresholdFactor', 1); % numercial differenciation will produce outliers 
-dr = smoothdata(dr);
+% Smoothed
+dr_Smoothed = filloutliers(dr,'clip','movmedian', 10, 'ThresholdFactor', 1); % numercial differenciation will produce outliers 
+dr_Smoothed = smoothdata(dr_Smoothed);
 
-ddr = diff(dr, 1, 2)/dt;    
-ddr = smoothdata(ddr);
+%% Second difference
+% Origin
+ddr = diff(dr_Smoothed, 1, 2)/dt;
+% Smoothed
+ddr_Smoothed = smoothdata(ddr);
 
-dr = [zeros(1, 1) dr]; % diff() will result in 1 missing data, resize it
-ddr = [zeros(1, 2) ddr]; % diff() will result in 1 missing data, resize it
+%% Resize
+dr = AddZerosToFrontOfArray(dr, 1);
+ddr = AddZerosToFrontOfArray(ddr, 2);
+dr_Smoothed = AddZerosToFrontOfArray(dr_Smoothed, 1);
+ddr_Smoothed = AddZerosToFrontOfArray(ddr_Smoothed, 2);
 
+%% Store results
 tr.r(i,:) = r;
 tr.dr(i,:) = dr;
 tr.ddr(i,:) = ddr;
+
+tr.dr_Smoothed(i,:) = dr_Smoothed;
+tr.ddr_Smoothed(i,:) = ddr_Smoothed;
 end
 
+%% Plot
 figure
 plot(t, tr.r)
+title('r(t)')
 figure
 plot(t, tr.dr)
+title('r''(t)')
+figure
+plot(t, tr.dr_Smoothed)
+title('r''(t), smoothed')
 figure
 plot(t, tr.ddr)
+title('r''''(t)')
+figure
+plot(t, tr.ddr_Smoothed)
+title('r''''(t), smoothed')
+
+%% Functions
+function arr = AddZerosToFrontOfArray(arr, num) % diff() will result missing data, resize it
+    arr = [zeros(1, num) arr];
+end
